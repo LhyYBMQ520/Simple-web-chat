a1ecd5e2adf83d6aac0d27044972bd6fc73de5dc
L菌 <176322200+LhyYBMQ520@users.noreply.github.com>
2026-05-26 19:27:15 +0800

    修复bug
    屏幕共享时没有声音

--- public/js/webrtc-module.js
+++ public/js/webrtc-module.js
@@ -107,6 +107,16 @@
             frameRate: { ideal: 30 }
           },
           audio: true
+        }).then(function(screenStream) {
+          return navigator.mediaDevices.getUserMedia({
+            audio: { echoCancellation: true, noiseSuppression: true, autoGainControl: true },
+            video: false
+          }).then(function(micStream) {
+            micStream.getAudioTracks().forEach(function(track) {
+              screenStream.addTrack(track);
+            });
+            return screenStream;
+          });
         });
       }
 
